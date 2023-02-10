fb9a09e8b448fd9a8ebf630a6185967ccc9a3cb8
EmmJim <emmsa.jo@hotmail.com>
2023-02-10 10:12:32 -0600

    Google Sign In Implemented

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req, res) => {
     const {correo, password} = req.body;
@@ -47,4 +48,54 @@ const login = async (req, res) => {
     
 }
 
-module.exports = { login }
+const googleSignIn = async(req, res) => {
+    const {id_token} = req.body;
+
+    try {
+        
+        const {nombre, img, correo} = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({correo});
+
+        if(!usuario){
+            //Crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        //Si el usuario en BD
+        if(!usuario.estado){
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        //Generar el JWT
+        const token = await generarJWT(usuario.id);
+
+
+
+        res.json({
+            usuario,
+            token
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+
+    
+}
+
+module.exports = { login, googleSignIn }
